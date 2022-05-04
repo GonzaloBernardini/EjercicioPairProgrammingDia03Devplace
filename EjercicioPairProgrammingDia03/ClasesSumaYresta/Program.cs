c7c823699fcf34bcd3bf9490625673cb992d8b7e
GonzaloBernardini <gbernardini58@gmail.com>
2022-05-04 18:01:43 -0300

    CommitInicial

---
+++ EjercicioPairProgrammingDia03/ClasesSumaYresta/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClasesSumaYresta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Suma sm = new Suma();
+            int r = 0;
+            sm.Operar(2,5,r);
+            Resta res1 = new Resta();
+            res1.Operar(10,5,r);
+           
+        }
+        public class Operacion 
+        {
+         protected int valor1 { get; set; }
+         protected int valor2 { get; set; }
+
+         protected int resultado;
+            public void  Operar(int valor1, int valor2, int resultado) 
+            {
+                
+            }
+        }
+        public class Suma : Operacion 
+        { 
+            public new void Operar(int valor1, int valor2, int resultado) 
+            { 
+                this.valor1 = valor1;
+                this.valor2 = valor2;
+                this.resultado = resultado;
+                resultado = valor1 + valor2;
+                Console.WriteLine($"El resultado es : {resultado}");
+                
+            }
+        }
+        public class Resta : Operacion 
+        { 
+            public new void Operar(int valor1, int valor2, int resultado) {
+                this.valor1 = valor1;
+                this.valor2 = valor2;
+                this.resultado = resultado;
+                resultado = valor1 - valor2;
+                Console.WriteLine($"El resultado es : {resultado}");
+
+            }
+        }
+    }
+}
---
+++ EjercicioPairProgrammingDia03/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EjercicioPairProgrammingDia03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto a1 = new Auto();
+            bool bandera = false;
+            do
+            {
+                try
+                {                  
+                    Console.WriteLine("Ingrese la marca del vehiculo");
+                    a1.MarcaVehiculo = Console.ReadLine();
+                    Console.WriteLine("Ingrese tipo de Combustible");
+                    a1.TipoCombustible = Console.ReadLine();
+                    Console.WriteLine("Ingrese cantidad de ruedas");
+                    a1.CantidadRuedas = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingrese cantidad de pasajeros");
+                    a1.CantidadPasajeros = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Tiene aire acondicionado?");
+                    a1.AireAcondicionado = Convert.ToBoolean(Console.ReadLine());
+                    a1.MostrarDatos(a1.MarcaVehiculo, a1.CantidadRuedas, a1.CantidadPasajeros);
+                    a1.MostrarDatosAuto(a1.TipoCombustible, a1.AireAcondicionado);
+                    a1.QuitarTecho();
+                    bandera = false;
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message,"Tipo de dato invalido");
+                    bandera = true;
+
+                }
+            } while(bandera == true);
+
+            
+            
+
+
+           
+
+        }
+
+        class Vehiculo
+        {
+            
+            public string MarcaVehiculo { get; set; }
+
+            public int CantidadRuedas { get; set; }
+
+            public int CantidadPasajeros { get; set; }
+
+
+            public void MostrarDatos(string m, int cr, int cp)
+            {
+                Console.WriteLine();
+            }
+        }
+        class Moto : Vehiculo
+        {
+            public string Cilindrada { get; set; }           
+
+            public void HacerWilly()
+            {
+                Console.WriteLine("Estoy haciendo willy");
+            }
+            public  void MostrarDatosMoto(string cl)
+            {
+                Console.WriteLine("Mi cilindrada es de: ");
+            }
+
+
+        }
+
+        class Auto : Vehiculo
+        {
+            public string TipoCombustible { get; set; }
+
+            public bool AireAcondicionado { get; set; }
+
+            public void QuitarTecho()
+            {
+                Console.WriteLine("Me hago descapotable");
+            }
+
+            public new void MostrarDatos(string m,int cr,int cp)
+            {
+                Console.WriteLine($"Marca :  {m} \n Cantidad de Ruedas : {cr} \n Cantidad de Pasajeros : {cp}");
+            }
+
+            public void MostrarDatosAuto(string tc,bool aa)
+            {
+                Console.WriteLine($" Tipo de Combustible : {tc} \n Aire Acondicionado : {aa}");
+            }
+
+
+        }
+        
+    }
+}
